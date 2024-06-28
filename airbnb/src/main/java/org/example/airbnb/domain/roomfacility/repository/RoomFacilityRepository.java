a6b792ecea14679411150b58e7fe04be24ec8e50
kkongdo <kkongdoes@naver.com>
2024-06-29 00:50:31 +0900

    #7 [BE] fix: write house facility Query in RoomFacilityRepository

--- airbnb/src/main/java/org/example/airbnb/domain/roomfacility/repository/RoomFacilityRepository.java
+++ airbnb/src/main/java/org/example/airbnb/domain/roomfacility/repository/RoomFacilityRepository.java
@@ -1,9 +1,16 @@
 package org.example.airbnb.domain.roomfacility.repository;
 
+import org.example.airbnb.domain.facility.entity.Facility;
 import org.example.airbnb.domain.roomfacility.entity.RoomFacility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomFacilityRepository extends JpaRepository<RoomFacility,Long> {
+    @Query("SELECT RF FROM RoomFacility RF WHERE RF.room.id = :roomId")
+    List<RoomFacility> findFacilitiesByroomId(Long roomId);
+
 }
