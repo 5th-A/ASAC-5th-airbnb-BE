9b4af741503c87439918694983331a0d9f5c15b4
kkongdo <kkongdoes@naver.com>
2024-06-29 03:13:11 +0900

    #11 [BE] feat: create CommentRepository and implement native Query

--- airbnb/src/main/java/org/example/airbnb/domain/comment/repository/CommentRepository.java
+++ airbnb/src/main/java/org/example/airbnb/domain/comment/repository/CommentRepository.java
@@ -2,8 +2,15 @@ package org.example.airbnb.domain.comment.repository;
 
 import org.example.airbnb.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query(value = "SELECT * FROM COMMENT WHERE ROOM_ID = :roomId", nativeQuery = true)
+    List<Comment> findCommentByRoomId(Long roomId);
+    @Query(value = "SELECT ROUND(AVG(RATE),2) FROM COMMENT WHERE ROOM_ID = :roomId",nativeQuery = true)
+    Double findTotalAvg(Long roomId);
 }
