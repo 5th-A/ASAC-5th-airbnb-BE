1fe1ed1a3d852b2d050508f0b30b43d76cb43e85
kkongdo <kkongdoes@naver.com>
2024-06-29 13:43:59 +0900

    #15 [BE] chore: apply annotation date filed

--- airbnb/src/main/java/org/example/airbnb/domain/comment/dto/CommentRequestDto.java
+++ airbnb/src/main/java/org/example/airbnb/domain/comment/dto/CommentRequestDto.java
@@ -12,6 +12,6 @@ import java.time.LocalDateTime;
 public class CommentRequestDto {
     private Long userId;
     private Integer rate;
-    private LocalDate date;
+    private LocalDate date; // JpaAudting을 통해 자동으로 날짜를 넣어주지만 JSON Request로 받기 때문에 삭제하지 않고 넣어둠.
     private String msg;
 }
