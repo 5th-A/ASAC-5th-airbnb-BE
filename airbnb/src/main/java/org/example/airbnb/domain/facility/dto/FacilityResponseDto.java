3c82846db6f09a91c3612fb5b0f3bd4896f63557
kkongdo <kkongdoes@naver.com>
2024-06-30 01:47:06 +0900

    #18 [BE] refactor: FacilityResponseDto의 불변성을 보장하기 위해 생성자 주입 방식을 사용하여 리팩토링

--- airbnb/src/main/java/org/example/airbnb/domain/facility/dto/FacilityResponseDto.java
+++ airbnb/src/main/java/org/example/airbnb/domain/facility/dto/FacilityResponseDto.java
@@ -1,28 +1,24 @@
 package org.example.airbnb.domain.facility.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.example.airbnb.domain.facility.entity.Facility;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class FacilityResponseDto {
-    private Long id;
+    private final Long id;
     @JsonProperty(value = "icon")
-    private String icon;
+    private final String icon;
     @JsonProperty(value = "name")
-    private String name;
+    private final String name;
 
     public static FacilityResponseDto of(Facility facility){
-        FacilityResponseDto dto = new FacilityResponseDto();
-        dto.id = facility.getId();
-        dto.icon = facility.getIcon();
-        dto.name = facility.getName();
-        return dto;
+       return new FacilityResponseDto(
+                facility.getId(),
+                facility.getIcon(),
+                facility.getName()
+        );
     }
 }
