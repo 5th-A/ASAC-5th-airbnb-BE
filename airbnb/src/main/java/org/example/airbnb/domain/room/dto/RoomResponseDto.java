adff07b81323e1437e91fc48e9dc15a70afbef0a
kkongdo <kkongdoes@naver.com>
2024-06-30 01:43:40 +0900

    #18 [BE] refactor: RoomResponseDto의 불변성을 보장하기 위해 생성자 주입 방식을 사용하여 리팩토링

--- airbnb/src/main/java/org/example/airbnb/domain/room/dto/RoomResponseDto.java
+++ airbnb/src/main/java/org/example/airbnb/domain/room/dto/RoomResponseDto.java
@@ -1,42 +1,41 @@
 package org.example.airbnb.domain.room.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.example.airbnb.domain.category.dto.CategoryResponseDto;
 import org.example.airbnb.domain.facility.dto.FacilityResponseDto;
 import org.example.airbnb.domain.room.entity.Room;
 
 import java.util.List;
 
-@NoArgsConstructor
 @Getter
+@AllArgsConstructor
 public class RoomResponseDto {
-    private Long id;
-    private String address;
-    private String roomType;
-    private Boolean guestPrefer;
-    private Long price;
-    private Integer maximum;
-    private Integer beds;
-    private Integer bedRooms;
-    private Integer bathRooms;
-    private CategoryResponseDto category;
-    private List<FacilityResponseDto> facilities;
+    private final Long id;
+    private final String address;
+    private final String roomType;
+    private final Boolean guestPrefer;
+    private final Long price;
+    private final Integer maximum;
+    private final Integer beds;
+    private final Integer bedRooms;
+    private final Integer bathRooms;
+    private final CategoryResponseDto category;
+    private final List<FacilityResponseDto> facilities;
 
     public static RoomResponseDto of(Room room, CategoryResponseDto categoryDto, List<FacilityResponseDto> facilityResponseDto) {
-        RoomResponseDto dto = new RoomResponseDto();
-        dto.id = room.getId();
-        dto.address = room.getAddress();
-        dto.roomType = room.getRoomType();
-        dto.guestPrefer = room.getGuestPrefer();
-        dto.price = room.getPrice();
-        dto.maximum = room.getMaxGuest();
-        dto.beds = room.getBed();
-        dto.bedRooms = room.getBedroom();
-        dto.bathRooms = room.getBathroom();
-        dto.category = categoryDto;
-        dto.facilities = facilityResponseDto;
-        return dto;
+        return new RoomResponseDto(
+                room.getId(),
+                room.getAddress(),
+                room.getRoomType(),
+                room.getGuestPrefer(),
+                room.getPrice(),
+                room.getMaxGuest(),
+                room.getBed(),
+                room.getBedroom(),
+                room.getBathroom(),
+                categoryDto,
+                facilityResponseDto
+        );
     }
 }
