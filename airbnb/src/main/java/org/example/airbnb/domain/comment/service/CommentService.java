990e5a15302906c092e2393b63dcc9fac28f16ae
kkongdo <kkongdoes@naver.com>
2024-06-29 13:25:51 +0900

    #15 [BE] feat: implement saveComment method to save Comment ãin room

--- airbnb/src/main/java/org/example/airbnb/domain/comment/service/CommentService.java
+++ airbnb/src/main/java/org/example/airbnb/domain/comment/service/CommentService.java
@@ -1,14 +1,22 @@
 package org.example.airbnb.domain.comment.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.airbnb.domain.comment.dto.CommentDto;
+import org.example.airbnb.domain.comment.dto.CommentRequestDto;
 import org.example.airbnb.domain.comment.dto.CommentResponseDto;
 import org.example.airbnb.domain.comment.entity.Comment;
 import org.example.airbnb.domain.comment.repository.CommentRepository;
 import org.example.airbnb.domain.image.dto.ImageDto;
+import org.example.airbnb.domain.room.dto.RoomDto;
+import org.example.airbnb.domain.room.dto.RoomResponseDto;
+import org.example.airbnb.domain.room.entity.Room;
 import org.example.airbnb.domain.room.repository.RoomRepository;
 import org.example.airbnb.exception.CommentException;
 import org.example.airbnb.exception.CustomRuntimeException;
+import org.example.airbnb.exception.RoomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,6 +27,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final RoomRepository roomRepository;
 
     public CommentResponseDto findCommentByRoomId(Long roomId) {
         List<Comment> comments = commentRepository.findCommentByRoomId(roomId);
@@ -28,5 +37,16 @@ public class CommentService {
         Double avg = commentRepository.findTotalAvg(roomId);
         CommentResponseDto responseDto = CommentResponseDto.of(avg, commentDtos);
         return responseDto;
+    }
+
+    @Transactional
+    public CommentDto saveComment(Long roomId, CommentRequestDto commentRequestDto) {
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> {
+            throw new CustomRuntimeException(RoomException.ROOM_NOT_FOUND_EXCEPTION);
+        });
+        Comment comment = Comment.of(commentRequestDto, room);
+        Comment savedComment = commentRepository.save(comment);
+        CommentDto responseCommentDto = CommentDto.from(savedComment);
+        return responseCommentDto;
     }
 }
