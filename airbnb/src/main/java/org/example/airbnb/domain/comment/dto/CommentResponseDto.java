59b13e10fb526e4059c4619abf5bb3a53c1ff93c
kkongdo <kkongdoes@naver.com>
2024-06-30 01:45:26 +0900

    #18 [BE] refactor: CommentResponseDto의 불변성을 보장하기 위해 생성자 주입 방식을 사용하여 리팩토링

--- airbnb/src/main/java/org/example/airbnb/domain/comment/dto/CommentResponseDto.java
+++ airbnb/src/main/java/org/example/airbnb/domain/comment/dto/CommentResponseDto.java
@@ -1,6 +1,7 @@
 package org.example.airbnb.domain.comment.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,18 +10,18 @@ import org.example.airbnb.domain.comment.entity.Comment;
 import java.time.LocalDate;
 import java.util.List;
 
-@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class CommentResponseDto {
     @JsonProperty(value = "average")
-    private Double averageRate;
+    private final Double averageRate;
     @JsonProperty(value = "comment")
-    private List<CommentDto> comments;
+    private final List<CommentDto> comments;
 
     public static CommentResponseDto of(Double averageRate, List<CommentDto> comments){
-        CommentResponseDto dto = new CommentResponseDto();
-        dto.averageRate = averageRate;
-        dto.comments = comments;
-        return dto;
+        return new CommentResponseDto(
+                averageRate,
+                comments
+        );
     }
 }
